7d69b0b71f5aa3277be3563e76e4788430662910
Justin Fowler <justinfowler1996@gmail.com>
2023-12-13 00:03:11 -0600

    Simplified

--- .eleventy.js
+++
@@ -1,4 +0,0 @@
-const pluginDate = require('eleventy-plugin-date');
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(pluginDate);
-};
--- readme.txt
+++
@@ -1,3 +0,0 @@
-Run these commands:
-npx eleventy --serve --watch
-npx tailwindcss -i ./tailwind.css -o ./_site/css/styles.css --watch
