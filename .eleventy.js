5361c03f46e02dcd969cfeb539f22d4d114fb56e
Justin Fowler <justinfowler1996@gmail.com>
2024-08-29 13:42:02 -0500

    it's a party

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("main.css")
+  eleventyConfig.addPassthroughCopy("drinks.css")
   eleventyConfig.setTemplateFormats(["md", "html", "jpeg", "ttf"])
 }
