2e79b354174f8f3e04d36951ed076a5ab055a006
Justin Fowler <justinfowler1996@gmail.com>
2024-11-17 10:17:31 -0600

    better update

--- update.sh
+++ update.sh
@@ -3,3 +3,4 @@ cd /home/justin/Developer/justin-dot-how
 git pull origin main  # Replace 'main' with your default branch if different
 /usr/bin/npm install  # Install any new dependencies
 /usr/bin/npm run eleventy  # Rebuild the site
+node webhook-server.js &
